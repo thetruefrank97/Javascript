74274f42f5b65b336efe0f4c7334f20deaceb025
fchavez <chavezfrancisco308@gmail.com>
2021-03-25 23:38:55 -0700

    another commit

---
+++ TPexamples/weatherApp/public/app.js
@@ -0,0 +1,27 @@
+import { Weather } from "./weather.js";
+import { UI } from "./ui.js";
+//Init weather object
+const weather = new Weather("Merida");
+const ui = new UI();
+document.addEventListener("DOMContentLoaded", getWeather);
+//Change location event
+document.getElementById("w-change-btn").addEventListener("click", (event) => {
+    const city = document.getElementById("city").value;
+    weather.changeLocation(city);
+    const modal = document.querySelector(".modal");
+    const modalBackdrop = document.querySelector(".modal-backdrop");
+    // Get and display weather
+    getWeather();
+    //Close modal
+    modal.classList.remove("show");
+    modalBackdrop.classList.remove("show");
+});
+function getWeather() {
+    weather.getWeather()
+        .then(response => {
+        ui.paint(response);
+    })
+        .catch(error => {
+        console.log(error);
+    });
+}
