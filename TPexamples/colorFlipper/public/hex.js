9dbf19ebb9f25dbb3d8431d05f92f307c2585d97
fchavez <chavezfrancisco308@gmail.com>
2021-03-27 18:52:00 -0700

    commit

--- TPexamples/colorFlipper/public/hex.js
+++ TPexamples/colorFlipper/public/hex.js
@@ -1,1 +1,15 @@
 "use strict";
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+btn.addEventListener("click", function () {
+    let hexColor = "#";
+    for (let x = 0; x < 6; x++) {
+        hexColor = hexColor + hex[getRandomNumber()];
+    }
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+});
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length);
+}
